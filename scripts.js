fdb8ca4784888de92d8a7496f74ed5b99599a930
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-03-14 01:47:26 -0400

    stream utilizes mic and camera, stopMyFeed function turns off stream

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const videoEl = document.querySelector('#my-video');
 let stream = null; // Init stream bar so we can use it anywhere
 
 const constraints = {
@@ -16,6 +17,27 @@ const getMicAndCamera = async (e) => {
     }
 };
 
+const showMyFeed = (e) => {
+    videoEl.srcObject = stream; // this will set our MediaStream (stream) to our <video />
+    const tracks = stream.getTracks();
+    console.log(tracks);
+}
+
+const stopMyFeed = () => {
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => {
+        track.stop(); // disassociates the track with the source
+    })
+}
+
 document.querySelector('#share').addEventListener('click', (e) => {
     getMicAndCamera(e);
-})
+});
+
+document.querySelector('#show-video').addEventListener('click', (e) => {
+    showMyFeed(e);
+});
+
+document.querySelector('#stop-video').addEventListener('click', (e) => {
+    stopMyFeed(e);
+});
